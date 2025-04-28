a20dc04d525ee299df90bdf2098bd8a1a9c5f95e
Igor Alexeenko <>
2025-04-28 20:32:25 +0300

    Add Express as dependency and init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world");
+import express, { Request, Response } from "express";
+
+const server = express();
+const PORT = 4000;
+
+server.get("/", function(req: Request, res: Response) {
+  res.status(200).json("Hello from backend");
+});
+
+server.listen(PORT, function() {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
