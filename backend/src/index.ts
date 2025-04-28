161e086cbb8619001fa97c4737b4a9e6ea98a9eb
Igor Alexeenko <>
2025-04-28 20:41:57 +0300

    Backend: Add /messages handle and welcome message from bot

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response } from "express";
 
+type Message = {
+  "id": number,
+  "username": string,
+  "text": string,
+  "timestamp": string,
+};
+
 const server = express();
 const PORT = 4000;
 
+const messages:Message[] = [];
+
 server.get("/", function(req: Request, res: Response) {
   res.status(200).json("Hello from backend");
+});
+
+server.get("/messages", function(req: Request, res: Response) {
+  res.status(200).json([...messages, {
+    "id": messages.length,
+    "username": "Bot 🤖",
+    "text": "Welcome to chat",
+    "timestamp": new Date().toISOString(),
+  }]);
 });
 
 server.listen(PORT, function() {
