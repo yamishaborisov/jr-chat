682acd10cfebaae94f251ca951a48c3b5ca3cebf
Igor Alexeenko <>
2025-04-28 21:00:20 +0300

    Render messages list on frontend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,38 @@
+{
+  const container = document.querySelector(".messages");
 
+  function renderMessages(messages) {
+    for (const message of messages) {
+      const messageElement = document.createElement("article");
+      messageElement.className = "message";
+
+      messageElement.innerHTML = `
+        <div class="message-author">${message.username}</div>
+        <button class="message-control"></button>
+        <p class="message-text">${message.text}</p>
+        <time>${message.timestamp}</time>
+      `;
+
+      container.appendChild(messageElement);
+    }
+  }
+
+  function initChat() {
+    fetch("http://localhost:4000/messages", {
+      method: "GET",
+    })
+      .then(function (messagesResponse) {
+        if (messagesResponse.status !== 200) {
+          throw new Error("Couldn't get messages from server");
+        }
+
+        return messagesResponse.json();
+      })
+      .then(function (messagesList) {
+        console.log(messagesList);
+        renderMessages(messagesList);
+      });
+  }
+
+  initChat();
+}
