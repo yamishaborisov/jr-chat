1e3024b24569d9d402c0e109cf210043f3afc791
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-05-05 20:13:12 +0300

    Backend: add POST request for new messages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,19 +13,60 @@ const PORT = 4000;
 
 const messages:Message[] = [];
 
+function* infiniteSequence() {
+  let i = 0;
+  while (true) {
+    yield ++i;
+  }
+}
+
+const idIterator = infiniteSequence();
+
 server.use(cors());
+server.use(express.json());
 
 server.get("/", function(req: Request, res: Response) {
   res.status(200).json("Hello from backend");
 });
 
 server.get("/messages", function(req: Request, res: Response) {
-  res.status(200).json([...messages, {
-    "id": messages.length,
-    "username": "Bot 🤖",
-    "text": "Welcome to chat",
-    "timestamp": new Date().toISOString(),
-  }]);
+  res.status(200).json([...messages]);
+});
+
+server.post("/messages", function(req: Request, res: Response) {
+  const { username, text } = req.body;
+
+  // 2 Стратегии валидации
+  //   1. Проверяются все ошибки и отправляются скопом
+  //   2. Проверка останавливается на первой попавшейся ошибке и отправляется эта ошибка
+
+  // *Некрасивенько, что в одном if проводятся сразу все проверки username
+  // потому что сложно сформировать адекватное сообщение об ошибке
+  if (typeof username !== "string" || username.length < 2 || username.length > 50) {
+    res.status(400).send({
+      message: "Incorrect username",
+    });
+
+    return;
+  }
+
+  if (typeof text !== "string" || text.length < 1 || text.length > 500) {
+    res.status(400).send({
+      message: "Incorrect message text",
+    });
+
+    return;
+  }
+
+  const newMessage = {
+    id: idIterator.next().value as number,
+    text,
+    timestamp: new Date().toISOString(),
+    username,
+  };
+
+  messages.push(newMessage);
+  res.status(201).send(newMessage);
 });
 
 server.listen(PORT, function() {
