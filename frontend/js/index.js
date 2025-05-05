293adaad1087b61b309651f7c5626f02d3f074aa
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-05-05 21:08:17 +0300

    Frontend: chat styling, messages senging

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,23 +1,62 @@
+/**
+ * Требования:
+ * - Прозрачная обратная связь — в любой момент времени пользователь
+ *   должен понимать что происходит с интерфейсомы
+ *   - Можно ли писать текст сообщения?
+ *   - Валидно ли сообщение, которое он отправляет и можно ли его отправить?
+ *   - После отправки 
+ *    - началась ли отправка?
+ *    - пришло ли сообщение на сервер? удачно ли?
+ *    - [отображение сообщения в списке]
+ * 
+ * 1. Я нажал на кнопку отправить
+ * 2. На сервер ушел POST-запрос
+ * 3. Сервер обработал этот запрос
+ * 4. Вернул мне ответ
+ * 5. Я обработал ответ, понял есть ли ошибка
+ * 6. Если нет ошибки — показал это
+ * 6.1 Если есть ошибка — показал это
+ * 
+ * Хорошо бы дать возможность пользователю не отправлять одно и то же сообщение
+ * несколько раз
+ * 
+ * Способы обратной связи 
+ * 1. Ничего не делать
+ * 2. Все заблокировать
+ *   1. Заблокировать поле ввода и кнопку и поменять текст на кнопке
+ *   2. Если удачно — разблокировать и вернуть текст обратно, очистить форму и отобразить обновленный список сообщений
+ *   3. Если ошибка — разблокировать и вернуть текст обратно, не сбрасывать форму и показать ошибку
+ * 3. Optimistic UI
+ *   1. Мгновенно обновляет список сообщений и показывает наше сообщение в списке
+ *      Очищает форму и дает возможность отправить новое сообщение
+ *      Вновь созданному сообщению добавляет визуальный индикатор о его состоянии
+ */
+
+
 {
   const container = document.querySelector(".messages");
 
   function renderMessages(messages) {
+    container.innerHTML = "";
+
     for (const message of messages) {
       const messageElement = document.createElement("article");
       messageElement.className = "message";
 
       messageElement.innerHTML = `
-        <div class="message-author">${message.username}</div>
-        <button class="message-control"></button>
+        <div class="message-header">
+          <div class="message-author">${message.username}</div>
+          <button class="message-control"></button>
+        </div>
         <p class="message-text">${message.text}</p>
-        <time>${message.timestamp}</time>
+        <time class="message-time">${message.timestamp}</time>
       `;
 
       container.appendChild(messageElement);
     }
   }
 
-  function initChat() {
+  function getMessages() {
     fetch("http://localhost:4000/messages", {
       method: "GET",
     })
@@ -32,6 +71,54 @@
         console.log(messagesList);
         renderMessages(messagesList);
       });
+  }
+
+  function initForm() {
+    const formContainer = document.querySelector("form");
+
+    const formTextField = formContainer.querySelector("textarea");
+    const formSubmitButton = formContainer.querySelector("button");
+
+    formContainer.onsubmit = function(evt) {
+      evt.preventDefault();
+      const formData = new FormData(evt.target);
+
+      const messageData = {
+        username: formData.get("username"),
+        text: formData.get("text"),
+      };
+
+      formTextField.disabled = true;
+      formSubmitButton.disabled = true;
+      formSubmitButton.textContent = "Сообщение отправляется...";
+
+      fetch("http://localhost:4000/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(messageData),
+      })
+        .then(function(newMessageResponse) {
+          console.log(newMessageResponse.status);
+
+          if (newMessageResponse.status !== 200) {
+            //
+          }
+
+          formTextField.disabled = false;
+          formTextField.value = "";
+          formSubmitButton.disabled = false;
+          formSubmitButton.textContent = "Отправить";
+
+          getMessages();
+        });
+    }
+  }
+
+  function initChat() {
+    getMessages();
+    initForm();
   }
 
   initChat();
