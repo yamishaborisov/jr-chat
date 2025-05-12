d3f04698e6e70e3190e1aaf3f9d088711b439e99
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-05-12 21:18:23 +0300

    Frontend: username form

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -30,22 +30,37 @@
  *   1. Мгновенно обновляет список сообщений и показывает наше сообщение в списке
  *      Очищает форму и дает возможность отправить новое сообщение
  *      Вновь созданному сообщению добавляет визуальный индикатор о его состоянии
+ * 
+ * 
+ * 
+ * 
+ * Ввод имени пользователя
+ * - [x] изначально имя пользователя не задано - null
+ * 
+ * - [x] если имени пользователя нет — показываем соответствующий экран
+ * - [ ] при вводе имя сохраняется в localStorage
+ * - [ ] введенное имя отправляется в каждом сообщении
+ * 
+ * - при рендеринге списка сообщений, если имя пользователя совпадает с 
+ *   введенным именем, это сообщение показывается справа
  */
 
 
 {
-  // Состояние
-  const messages = [];
-  let atBottom = false;
+  const USERNAME_REC = "username";
 
-  const container = document.querySelector(".messages");
+  let username = null;
+
+  const chatContainer = document.querySelector(".messages");
+  const usernameContainer = document.querySelector(".username");
 
   function renderMessages(messages) {
-    container.innerHTML = "";
+    chatContainer.innerHTML = "";
 
     for (const message of messages) {
       const messageElement = document.createElement("article");
       messageElement.className = "message";
+      messageElement.classList.toggle("message-mine", username === message.username);
 
       messageElement.innerHTML = `
         <div class="message-header">
@@ -56,7 +71,7 @@
         <time class="message-time">${message.timestamp}</time>
       `;
 
-      container.appendChild(messageElement);
+      chatContainer.appendChild(messageElement);
     }
   }
 
@@ -81,17 +96,21 @@
   }
 
   function scrollToBottom() {
-    container.scrollTop = container.scrollHeight;
+    chatContainer.scrollTop = chatContainer.scrollHeight;
   }
 
   function initForm() {
-    const formContainer = document.querySelector("form");
+    const formContainer = document.querySelector("#message-form");
 
     const formTextField = formContainer.querySelector("textarea");
     const formSubmitButton = formContainer.querySelector("button");
 
+    const usernameField = formContainer.querySelector("input[name=username]");
+    usernameField.value = username;
+
     formContainer.onsubmit = function(evt) {
       evt.preventDefault();
+
       const formData = new FormData(evt.target);
 
       const messageData = {
@@ -139,13 +158,55 @@
     // Как правильно скроллить?
     // - Когда мы сами отправили [новое сообщение]
     // - Когда мы находимся внизу списка и пришло [новое сообщение]
-
-    // - Когда мы находимся не снизу списка и пришло [новое сообщние], 
-    //   нужна индикация
+    // - Когда мы только загрузили страницу
 
     // | | | | | | | | | |
     //        | ||  ||| |
   }
 
-  initChat();
+  // Форма может жить в двух состояниях — модальное окно показано и модальное окно
+  // не показано
+  // Режим когда окно не показано может быть инициализирован после того как 
+  // имя пользователя было введено
+  // При создании функционала некоего модуля, который описывает работу
+  // с DOM, нужно описывать не только инициализацию, но и "разрушение"
+  // этого модуля
+  function initUsernameForm() {
+    const usernameForm = usernameContainer.querySelector("form");
+
+    usernameForm.onsubmit = function(evt) {
+      evt.preventDefault();
+
+      const formElement = evt.target;
+      const formData = new FormData(formElement);
+      const enteredUsername = formData.get("username");
+
+      localStorage.setItem(USERNAME_REC, enteredUsername);
+
+      usernameContainer.close();
+      usernameForm.onsubmit = null;
+
+      initApp();
+    };
+
+    usernameContainer.showModal();
+  }
+
+  // Модальное приложение
+  // Модальность — зависимость от состояния
+  // В нашем случае режим переключается наличием username
+  // - есть username — режим чата
+  // - нет username — режим ввода username
+  function initApp() {
+    username = localStorage.getItem(USERNAME_REC);
+
+    if (username === null) {
+      initUsernameForm();
+      return;
+    }
+
+    initChat();
+  }
+
+  initApp();
 }
