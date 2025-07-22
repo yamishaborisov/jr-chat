e8b68d81e4e236bb3fd2b83de14e531470025ff5
yamishaborisov <borisov3676@gmail.com>
2025-07-22 19:58:53 +0300

    add docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ type Message = {
 };
 
 const server = express();
-const PORT = 4000;
+const PORT = process.env.APP_PORT || 4000;
 
 const messages: Message[] = [];
 
@@ -30,7 +30,13 @@ server.get('/', function (req: Request, res: Response) {
 });
 
 server.get('/messages', function (req: Request, res: Response) {
-	res.status(200).json([...messages]);
+	res
+		.status(200)
+		.json(
+			[...messages].filter(
+				m => Date.now() - +new Date(m.timestamp) < 1000 * 60 * 60 * 24 * 3
+			)
+		);
 });
 
 server.post('/messages', function (req: Request, res: Response) {
